3d464b065ba8de42f6e5452b4372d33122c4c458
liangjiangtao <liangjiangtao@hongyuanshidai.cn>
2018-04-02 19:07:54 +0800

    添加插件修改class字节码

--- app/src/main/java/com/moonljt521/gradleplugindemo/MainActivity.java
+++ app/src/main/java/com/moonljt521/gradleplugindemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.moonljt521.gradleplugindemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.gradlePluginEventTrackingBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
